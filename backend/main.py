ecd087bf1f16d8b713aded4a0b9e86cf65ba52c4
Vlados <vladgrigorea@gmail.com>
2025-10-19 08:29:55 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -107,7 +107,7 @@ app.include_router(club_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:3000", "http://localhost:8080"],
+    allow_origins=["http://127.0.0.1:5500", "http://localhost:3000", "http://localhost:8080", "http://localhost:8081"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
